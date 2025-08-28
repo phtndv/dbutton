dee5442a0cbed1441763a5a521fd2b7e279275ba
rayomarblez <rayomarblezh@protonmail.com>
2025-08-28 00:37:51 -0900

    go

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-# This is a minimal setup.py that uses pyproject.toml for configuration
-# All package metadata and dependencies are now in pyproject.toml
-
-if __name__ == "__main__":
-    setup()
